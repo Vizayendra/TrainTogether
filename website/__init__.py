ed3e9bbc480bf9ce2c5cb1bba51b83e1e9234346
Akshathaa <akshathaa1404@gmail.com>
2025-09-08 04:21:51 +0800

    login, logout & database

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -8,7 +10,7 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dfdfdffd'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{path.join(path.dirname(__file__), DB_NAME)}'
     db.init_app(app)
 
     from .views import views
@@ -17,4 +19,24 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
     return app
+
+
+def create_database(app):
+    db_path = path.join(path.dirname(__file__), DB_NAME)
+    if not path.exists(db_path):
+        with app.app_context():
+            db.create_all()
+            print(f'Created Database at {db_path}')
